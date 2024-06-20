6823364803f9c7a9c050347aee4e22ca5208c5a9
Vivian969 <vivianwang969@gmail.com>
2024-06-20 10:56:00 +0800

    test-jenkins

---
+++ src/main/java/org/example/practice/controller/test.java
@@ -0,0 +1,16 @@
+package org.example.practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class test {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String testJenkins(){
+        System.out.println("Testing Jenkins");
+        return "Jenkins tested.";
+    }
+}
